771ec960b52a58ed66bb1930cd450f2d269afe2c
chongxian <chongxian@easystore.co>
2020-07-23 00:50:27 +0800

    Force secure

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+if (env('APP_ENV') === 'production') {
+    URL::forceSchema('https');
+}
 
 Route::get('/', function () {
     return view('welcome');
