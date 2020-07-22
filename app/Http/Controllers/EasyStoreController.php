3a6965ee4f6e768e4487a4cb4dcfaa3d9b8ecb5b
chongxian <chongxian@easystore.co>
2020-07-22 23:38:15 +0800

    Json decode result

--- app/Http/Controllers/EasyStoreController.php
+++ app/Http/Controllers/EasyStoreController.php
@@ -93,6 +93,8 @@ class EasyStoreController extends Controller
         $result = curl_exec($ch);
         curl_close($ch);
 
+        $result = json_decode($result);
+
         $this->slack_say("#cx", json_encode([
             "message" => "Calling Access token endpoint",
             "url" => $url,
