d034e9a400c7f0916c20148b074bdc3bb9416718
chongxian <chongxian@easystore.co>
2020-07-23 14:20:07 +0800

    Add slack log

--- app/Http/Controllers/EasyStoreController.php
+++ app/Http/Controllers/EasyStoreController.php
@@ -194,12 +194,17 @@ class EasyStoreController extends Controller
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, 'EasyStore-Access-Token: ' . $shop->access_token);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['EasyStore-Access-Token' => $shop->access_token]);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
         $result = curl_exec($ch);
         curl_close($ch);
+
+        $this->slack_say("#cx", json_encode([
+            'creating' => "Webhook",
+            'result' => json_encode($result, true)
+        ]));
 
     }
 
